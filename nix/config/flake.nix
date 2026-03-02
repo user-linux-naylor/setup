281807608d2577728a866d22dd1ebe183167a16d
Jhonatan <255996669+user-linux-naylor@users.noreply.github.com>
2026-03-02 19:36:56 -0300

    Create nix/config/flake.nix

---
+++ nix/config/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Environment C/C++";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      
+      system = "aarch64-linux"; 
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ 
+          pkgs.clib 
+          pkgs.clang
+          pkgs.vcpkg
+          pkgs.gnumake
+        ];
+      };
+    };
+}
