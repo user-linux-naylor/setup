8934c905912b9f666d7d6b7a94c8b865ab5acc26
Jhonatan <255996669+user-linux-naylor@users.noreply.github.com>
2026-03-16 17:51:26 +0000

    Update packages.sh

--- termux/config/packages.sh
+++ termux/config/packages.sh
@@ -5,3 +5,11 @@ pkg install cmake
 pkg install make
 pkg install clib
 pkg install git
+
+git clone https://github.com/user-linux-naylor/vcpkg.git
+
+cd vcpkg && ./bootstrap-vcpkg.sh
+
+export VCPKG_ROOT=/path/to/vcpkg
+
+export PATH=$VCPKG_ROOT:$PATH
